bbf5e7497fa21c582bbd4cf57d7f4a014de49d78
Casey Zacney <czacney@gmail.com>
2020-04-15 20:57:24 -0400

    Updates

---
+++ precache-manifest.35c351f89086b4557463cd7dcfea5aa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9416ed731aba76f4ac63e68027904245",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "38e6d6dba3812934d606",
+    "url": "/ReactPortfolio/static/css/main.c6a6269d.chunk.css"
+  },
+  {
+    "revision": "424ddcb090efcb3aff04",
+    "url": "/ReactPortfolio/static/js/2.edf9fcb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.edf9fcb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e6d6dba3812934d606",
+    "url": "/ReactPortfolio/static/js/main.1b260b8a.chunk.js"
+  },
+  {
+    "revision": "95d34d4ed30bb33b384d",
+    "url": "/ReactPortfolio/static/js/runtime-main.5fd60e07.js"
+  },
+  {
+    "revision": "0528cf1425e822849bff7031c33fb629",
+    "url": "/ReactPortfolio/static/media/background.0528cf14.png"
+  },
+  {
+    "revision": "dfac1c293ccd0ac1f45ff4997afe4169",
+    "url": "/ReactPortfolio/static/media/me.dfac1c29.png"
+  }
+]);
